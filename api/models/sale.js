1fb9cd77792e6745762ae5171b9b54662fa5a783
Wojciech Bieńkowski <bienowsiw@gmail.com>
2025-01-12 18:46:30 +0100

    wersja4

--- api/models/sale.js
+++ api/models/sale.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
 
-// Zmiana typu car_id i client_id na ObjectId, żeby móc używać populate
 const saleSchema = new mongoose.Schema({
-    car_data: { type: mongoose.Schema.Types.ObjectId, ref: 'cars', required: true },   // Referencja do kolekcji Car
-    client_data: { type: mongoose.Schema.Types.ObjectId, ref: 'clients', required: true }, // Referencja do kolekcji Client (lub Customer)
-    date: { type: String, required: true },        // Data sprzedaży
-    price: { type: Number, required: true },      // Cena sprzedaży
-    status: { type: String, required: true }      // Status sprzedaży (np. "completed", "pending")
+    car_data: { type: mongoose.Schema.Types.ObjectId, ref: 'cars', required: true },
+    client_data: { type: mongoose.Schema.Types.ObjectId, ref: 'clients', required: true },
+    date: { type: String, required: true },
+    price: { type: Number, required: true },
+    status: { type: String, required: true }      
 });
 
 module.exports = mongoose.model('Sale', saleSchema);
